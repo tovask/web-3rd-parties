51fc23ba237551059cd2af3c4d862024e9a75599
Levente Kováts <22732484+tovask@users.noreply.github.com>
2019-05-03 16:57:40 +0000

    install OpenWPM 14
    global install 8
    
    pip 4 (additional files)

--- openwpm_setup.py
+++ openwpm_setup.py
@@ -1,7 +1,9 @@
 from distutils.core import setup;
 from setuptools import find_packages
+import os
+
 setup(name='OpenWPM',
       version='0.1.0',
-      package_data = {'': ['*']},
       packages=find_packages(exclude=['OpenWPM.test']),
-      )
+      package_data = {'OpenWPM': [ os.path.join(root[len('OpenWPM/'):],filename) for root, dirs, files in os.walk ("OpenWPM") for filename in files ] },
+    )
