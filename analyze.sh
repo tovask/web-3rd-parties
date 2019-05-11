fef0a9c90c11cb92ce4af8d2c03b497df743cc52
Levente Kováts <22732484+tovask@users.noreply.github.com>
2019-05-11 07:08:27 +0000

    small changes in the analizer script

--- analyze.sh
+++ analyze.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -e
-set -x
+#set -x
 
 # https://extendsclass.com/sqlite-browser.html
 
-db_file=$(find ~ -maxdepth 1 | grep openwpm_results | sort -r | head -n 1)/crawl-data.sqlite
-echo $db_file
+db_file=~/openwpm_results/crawl-data.sqlite
+if [ ! -f $db_file ]; then
+	db_file=$(find ~ -maxdepth 1 | grep openwpm_results | sort -r | head -n 1)/crawl-data.sqlite
+fi
+echo using database $db_file
 
 python << EOF
 import sqlite3
@@ -21,7 +24,7 @@ insert_cursor.execute('CREATE TABLE IF NOT EXISTS third_party_analyze ( url TEXT
 #conn.commit()
 
 # all_rows = cursor.fetchall()
-for row in select_cursor.execute('SELECT url , top_level_url FROM http_requests limit 50;'):
+for row in select_cursor.execute('SELECT url , top_level_url FROM http_requests;'):
 	#print row
 	#print(urlparse(row[0]).netloc,"\t",urlparse(row[1]).netloc)
 	is_third_party = ( urlparse(row[0]).hostname != urlparse(row[1]).hostname ) and ( urlparse(row[1]).hostname != None )
@@ -37,25 +40,31 @@ conn.close()
 EOF
 
 # export instrumented data to a html file (an ugly way...)
-htmlfilename=export.html
-echo "<html><body>" >$htmlfilename
+html_file=$(dirname $db_file)/export.html
+echo exporting to $html_file
+echo "<html><body>" >$html_file
 for table in $(echo ".tables" | sqlite3 $db_file); do
 	#break
-	echo -e "\n<hr>" >>$htmlfilename
-	echo "<h1>${table}</h1>" >>$htmlfilename
-	echo "<table border=1>" >>$htmlfilename
-	echo -e ".headers ON\n.mode html\nSELECT * FROM $table;" | sqlite3 $db_file >>$htmlfilename
-	echo "</table>" >>$htmlfilename
+	echo -e "\n<hr>" >>$html_file
+	echo "<h1>${table}</h1>" >>$html_file
+	echo "<table border=1>" >>$html_file
+	echo -e ".headers ON\n.mode html\nSELECT * FROM $table;" | sqlite3 $db_file >>$html_file
+	echo "</table>" >>$html_file
 done
-echo "</body></html>" >>$htmlfilename
+echo "</body></html>" >>$html_file
 
 #echo -e ".headers ON\nSELECT top_level_url, COUNT(*) FROM third_party_analyze WHERE is_third_party=0 GROUP BY top_level_url;" | sqlite3 $db_file
 echo
-echo -e ".headers ON\n.mode column\n.width 50 -14 -11 -22\nSELECT top_level_url, \
+echo -e ".headers ON\n.mode column\n.width 50 -14 -11 -22\n\
+		SELECT \
+			top_level_url, \
 			COUNT(*) AS 'total requests', \
 			COUNT(case when is_third_party = 1 then 1 else null end) AS 'third party', \
 			printf( '%6.2f%', COUNT(case when is_third_party = 1 then 1 else null end) * 100.0 / COUNT(*) ) AS 'third party requests %' \
-			FROM third_party_analyze GROUP BY top_level_url;" | sqlite3 $db_file
+		FROM third_party_analyze \
+		WHERE top_level_url != 'about:blank' AND top_level_url != 'undefined' \
+		GROUP BY top_level_url \
+		ORDER BY COUNT(case when is_third_party = 1 then 1 else null end) DESC;" | sqlite3 $db_file
 
 if [ -d ~/openwpm_results ]; then
 	mv ~/openwpm_results ~/.openwpm_results_$(date +"%Y%m%d%H%M%S%N")
