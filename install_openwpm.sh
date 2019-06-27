ff3dcf03e364cd8f7ab883e7452c8d51662e0682
Levente Kováts <22732484+tovask@users.noreply.github.com>
2019-06-27 14:39:27 +0000

    separate OpenWPM install steps, flake8

---
+++ install_openwpm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+orig_pwd=`pwd`
+cd `mktemp -d`
+
+git clone https://github.com/mozilla/OpenWPM.git --depth=1
+cd OpenWPM
+pip install --upgrade -r requirements.txt
+./install.sh --no-flash
+rm -rf automation/Extension/firefox/node_modules # not needed after the extension built, avoid copying these when installing the module
+cd ..
+
+cp $orig_pwd/openwpm_setup.py setup.py
+pip install . # install with setup.py, like a package, so it can be imported from anywhere
+
+pip show OpenWPM # verify the install
